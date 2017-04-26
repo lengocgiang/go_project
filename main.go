346c0e50eb7fa3176b0358cf7a7163e4d4792e5d
Giang Le Ngoc <gianglengoc.hut@gmail.com>
2017-04-26 15:19:18 +0700

    array, slice example

--- main.go
+++ main.go
@@ -1,8 +1,22 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+)
 
 func main() {
-	x := 500
-	fmt.Println(x)
+	arrayAndSlice()
+}
+
+//1 Array,slice
+func arrayAndSlice() {
+	// slice
+	slice1 := []int{1, 2, 4, 4}
+	slice2 := append(slice1, 5, 6, 7)
+	fmt.Println(slice1, slice2)
+
+	slice3 := []int{4, 5, 2, 4, 1}
+	slice4 := make([]int, 2)
+	copy(slice4, slice3)
+	fmt.Println(slice3, slice4)
 }
