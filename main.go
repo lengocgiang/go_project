299cf6a9cdbce50f5dd3fb2c875d594146d7c609
Giang Le Ngoc <gianglengoc.hut@gmail.com>
2017-04-27 14:01:58 +0700

    update

--- main.go
+++ main.go
@@ -5,7 +5,37 @@ import (
 )
 
 func main() {
-	mapExample()
+	//x := 5
+	//mapExample()
+	//closureExample()
+	//fmt.Println(fractional(x))
+	//deferExample()
+	//pointerExample()
+
+	//fmt.Println(half(4))
+	//fmt.Println(variadic(9, 12, 11, 42, 5, 3, 4, 5))
+
+	// Closure
+	/*
+		add := func(x, y int) int {
+			return x + y
+		}
+		fmt.Println(add(2, 3))
+	*/
+
+	var x *int
+	var y int
+	y = 0
+	x = &y
+
+	fmt.Println(x)
+	fmt.Println(&x)
+	fmt.Println(y)
+
+}
+
+func zero(xPtr *int) {
+	*xPtr = 0
 }
 
 //1 Array,slice
@@ -69,5 +99,88 @@ func mapExample() {
 	if el, ok := elements2["kk"]; !ok {
 		fmt.Println("null cmnr", el, ok)
 	}
+}
 
+// 3 Closure, Recursive
+func closureExample() {
+	x := 0
+	increament := func() int {
+		x++
+		return x
+	}
+	fmt.Println(increament())
+	fmt.Println(increament())
+}
+
+func fractional(x int) int {
+	if x == 0 {
+		return 1
+	}
+	return x * fractional(x-1)
+}
+
+// 4 defer, panic, recover
+func deferExample() {
+	defer second()
+	first()
+}
+
+func first() {
+	fmt.Println("First")
+}
+
+func second() {
+	fmt.Println("Second")
+}
+
+// 5 Pointer
+func pointerExample() {
+	var x *int // declare pointer x is a int pointer
+	var y int  // declare variable y is a int number
+	y = 0      // assign y value = 0
+	x = &y     // assign pointer x is
+
+	fmt.Println(x)  // gia tri cua x la dia chi cua y
+	fmt.Println(&y) // dia chi cua y
+	fmt.Println(*x) // lay gia tri cua y = 0
+	fmt.Println(y)  // gia tri cua y = 0
+
+	*x = 1 // gan gia tri con tro  x= 1
+
+	fmt.Println(*x) // gia tri cua x = 1
+	fmt.Println(y)  // gia tri cua y = 1
+}
+
+func half(x int) (float32, bool) {
+	if x%2 == 0 {
+		return float32(x / 2), true
+	}
+	return float32(x / 2), false
+}
+
+func variadic(args ...int) int {
+	min := args[0]
+	for _, value := range args {
+		if value < min {
+			min = value
+		}
+	}
+	return min
+}
+
+func deferWithPanic() {
+	defer func() {
+		str := recover()
+		fmt.Println(str)
+	}()
+	panic("PANIC")
+}
+
+func square(x *float64) {
+	*x = *x * *x
+}
+
+func swap(p1, p2 *int) {
+	// temp := new(int)
+	// temp = &p1
 }
